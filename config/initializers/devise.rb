8b85eb560b580dd6296776c4a89b7bfa9398ef4e
Matt T <MMT@Matts-MacBook-Pro.local>
2016-04-18 12:10:12 -0400

    Initial

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.omniauth :github, "ce810250842617027bb4", "d835d302ef92ea90b3eed826d5fb0994563aa368", scope: 'user,public_repo'
+  config.omniauth :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user,public_repo'
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
